cd66c37544f699ca00784827bddf41c567fa95d2
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-06-07 23:55:04 +0300

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,20 @@ $(document).ready(function () {
         $("input[type='text']").focus();
     });
 
+    $('.menu-bottom__text').click(function(){
+        if ($('.menu-bottom').css('display') == 'none') {
+            
+            $('.menu-bottom').slideDown();
+        } else {
+            
+            $('.menu-bottom').slideUp();
+        }
+    });
+    
     const icons = document.querySelectorAll('.icon');
     icons.forEach (icon => {  
         icon.addEventListener('click', (event) => {
             icon.classList.toggle("open");
         });
     });
-
 });
