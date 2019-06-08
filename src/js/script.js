4309b4e12c6a62c7904fc0b30aa009370c3e518b
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-06-08 16:01:40 +0300

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 
-    $(".search-form-block__button").click(function () {
-        $(".search-form-block, .search-input").toggleClass("search-active");
-        $("input[type='text']").focus();
-    });
-
+    if (screen.width > 993) {
+        $(".search-form-block__button").click(function () {
+            $(".search-form-block, .search-input").toggleClass("search-active");
+            $("input[type='text']").focus();
+        });
+    }
     $('.menu-bottom__text').click(function(){
         if ($('.menu-bottom').css('display') == 'none') {
             $('.menu-bottom').slideDown();
